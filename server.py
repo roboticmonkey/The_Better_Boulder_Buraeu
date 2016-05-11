dd98fa32b31b00e2591ca3608def563d80a0f2fb
aiden ward <aiden@MacBook-Pro-2.local>
2016-05-11 13:06:14 -0700

    server -doing the server thing. templates added

--- server.py
+++ server.py
@@ -1,3 +1,36 @@
-from flask import Flask
+""" Put something here"""
+
+from flask import Flask, render_template
+
+from flask_debugtoolbar import DebugToolbarExtension
+from model import connect_to_db
 
 app = Flask(__name__)
+
+app.secret_key = "something-secret"
+
+
+
+#############
+#The routes
+
+@app.route('/')
+def index():
+    """Homepage"""
+
+    return render_template('homepage.html')
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run()
