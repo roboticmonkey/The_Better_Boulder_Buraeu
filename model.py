b4ddd8851063166cfc1c74569b1f38e1af19730e
roboticmonkey <aiden.ward@gmail.com>
2016-05-17 12:53:49 -0700

    added data files and updated model.py

--- model.py
+++ model.py
@@ -35,71 +35,123 @@ class User(db.Model):
                                                         self.username,
                                                         self.email)
     
-
 class Location(db.Model):
-    """Climbing Location"""
-    
+    """Main Locations for Boulders"""
+
     __tablename__ = 'locations'
 
     location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     location_name = db.Column(db.String(100), nullable=False)
-    directions = db.Column(db.Text, nullable=True)
+    location_directions = db.Column(db.Text, nullable=True)
     latitude = db.Column(db.Integer, nullable=False)
     longitude = db.Column(db.Integer, nullable=False)
     location_description = db.Column(db.Text, nullable=False)
-    
+
     def __repr__(self):
         """Provide helpful representation when printed."""
 
         return "<Location: location_id=%s location_name=%s>" % (self.location_id,
                                                             self.location_name)
+
+
+
+class Sub_location(db.Model):
+    """Sub. locations for boulders"""
+
+    __tablename__ = 'sub_locations'
+
+    sub_location_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    sub_location_name = db.Column(db.String(100), nullable=False)
+    sub_location_directions = db.Column(db.Text, nullable=True)
+    sub_latitude = db.Column(db.Integer, nullable=True)
+    sub_longitude = db.Column(db.Integer, nullable=True)
+    sub_location_description = db.Column(db.Text, nullable=False)
+
+    location_id = db.Column(db.Integer, db.ForeignKey('location.location_id'))
+
+    #RELATIONSHIPS
+    location = db.relationship('Location', backref='Sub_location')
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Sub. Location: sub_location_id=%s sub_location_name=%s>" % (self.sub_location_id,
+                                                            self.sub_location_name)
+
+
+
+class Boulder(db.Model):
+    """Climbing Boulders"""
+    
+    __tablename__ = 'boulders'
+
+    boulder_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    boulder_name = db.Column(db.String(100), nullable=False)
+    boulder_directions = db.Column(db.Text, nullable=True)
+    latitude = db.Column(db.Integer, nullable=True)
+    longitude = db.Column(db.Integer, nullable=True)
+    boulder_description = db.Column(db.Text, nullable=False)
+    
+    location_id = db.Column(db.Integer, db.ForeignKey('location.location_id'))
+    sub_location_id = db.Column(db.Integer, db.ForeignKey('sub_location.location_id'), nullable=True)
+
+    #RELATIONSHIPS
+    location = db.relationship('Location', backref='Boulder')
+    sub_location = db.relationship('User', backref='Boulder')
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Boulder: boulder_id=%s boulder_name=%s>" % (self.boulder_id,
+                                                            self.boulder_name)
     
 
-class Location_rating(db.Model):
-    """Location ratings"""
+class Boulder_rating(db.Model):
+    """Boulder ratings"""
     
-    __tablename__ = 'location_ratings'
+    __tablename__ = 'boulder_ratings'
     
-    location_rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    location_rating = db.Column(db.Integer, nullable=False)
+    boulder_rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    boulder_rating = db.Column(db.Integer, nullable=False)
 
-    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
+    boulder_id = db.Column(db.Integer, db.ForeignKey('boulders.boulder_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     
     #RELATIONSHIPS
-    location = db.relationship('Location', backref='Location_rating')
-    user = db.relationship('User', backref='Location_rating')
+    boulder = db.relationship('Boulder', backref='Boulder_rating')
+    user = db.relationship('User', backref='Boulder_rating')
 
     def __repr__(self):
         """Provide helpful representation when printed."""
         
-        return ("<Location Rating: loc_rate_id=%s user_id=%s loc_id=%s loc_rate=%s>" 
-                % ( self.location_rating_id, self.user_id,
-                    self.location_id, self.location_rating) )
+        return ("<Boulder Rating: boulder_rate_id=%s user_id=%s boulder_id=%s boulder_rate=%s>" 
+                % ( self.boulder_rating_id, self.user_id,
+                    self.boulder_id, self.boulder_rating) )
  
 
-class Location_comment(db.Model):
-    """Location comments"""
+class Boulder_comment(db.Model):
+    """boulder comments"""
     
-    __tablename__ = 'location_comments'
+    __tablename__ = 'boulder_comments'
     
     
-    location_comment_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    location_comment = db.Column(db.Text, nullable=False)
-    location_datetime = db.Column(db.DateTime, nullable=False)
+    boulder_comment_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    boulder_comment = db.Column(db.Text, nullable=False)
+    boulder_datetime = db.Column(db.DateTime, nullable=False)
 
-    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
+    boulder_id = db.Column(db.Integer, db.ForeignKey('boulders.boulder_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     
     #RELATIONSHIPS
-    location = db.relationship('Location', backref='Location_comment')
-    user = db.relationship('User', backref='Location_comment')
+    boulder = db.relationship('Boulder', backref='Boulder_comment')
+    user = db.relationship('User', backref='Boulder_comment')
     
     def __repr__(self):
         """Provide helpful representation when printed."""
         
-        return ( "<Location Comment: location_comment_id=%s user_id=%s>" % 
-                (self.location_comment_id, self.user_id) )
+        return ( "<Boulder Comment: boulder_comment_id=%s user_id=%s>" % 
+                (self.boulder_comment_id, self.user_id) )
     
 
 class Route(db.Model):
@@ -111,17 +163,19 @@ class Route(db.Model):
     route_name = db.Column(db.String(200), nullable=False)
     difficulty_rate = db.Column(db.String(5), nullable=True)
     route_description = db.Column(db.Text, nullable=False)
+    route_directions = db.Column(db.Text, nullable=True)
+    route_protection = db.Column(db.Text, nullable=True)
 
-    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
+    boulder_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'))
   
     #RELATIONSHIPS
-    location = db.relationship('Location', backref='Route')
+    boulder = db.relationship('Boulder', backref='Route')
     
     def __repr__(self):
         """Provide helpful representation when printed."""
 
-        return ("<Routes: route_id=%s location_id=%s route_name=%s >" % 
-                (self.route_id, location_id, route_name) )
+        return ("<Routes: route_id=%s boulder_id=%s route_name=%s >" % 
+                (self.route_id, boulder_id, route_name) )
     
 
 class Route_rating(db.Model):
