83ae933a82ccdb0b1f3a7b1ca7b1135aed2ecd32
roboticmonkey <aiden.ward@gmail.com>
2016-05-22 14:00:09 -0700

    added some simple map feature on loc, subloc, boulder pages

--- static/search.js
+++ static/search.js
@@ -14,13 +14,47 @@ function displayResults(data) {
     // adding the info to the page here?
     var dataDestination = $("#search-results");
     // var each = data.data[0];
-    debugger;
+    // debugger;
+    // var markers = new L.MarkerClusterGroup();
+
     for (var i = 0; i < data.data.length; i++) {
         
         var each = data.data[i];
+        // var title = each.name;
+        // var latlong = [each.float(lat), each.float(lon)];
         var link = each.route+each.id;
         dataDestination.append('<li><a href="'+link+'">'+each.name+'</a></li>');
+
+        // var marker = L.marker(new latlong, title), {
+        //     icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}),
+        //     title: title
+        // });
+        
+        // marker.bindPopup(title);
+        // markers.addLayer(marker);
+
     }
+
+    // map.addLayer(markers);
+    
+
+
+
+    // for (var i = 0; i < addressPoints.length; i++) {
+    //     var a = addressPoints[i];
+    //     var title = a[2];
+    //     var marker = L.marker(new L.LatLng(a[0], a[1]), {
+    //         icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}),
+    //         title: title
+    //     });
+    //     marker.bindPopup(title);
+    //     markers.addLayer(marker);
+    // }
+
+    // map.addLayer(markers);
+
+
+    // }
     
 
 }
